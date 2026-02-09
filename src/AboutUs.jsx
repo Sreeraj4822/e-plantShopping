8f004397200d69707ec52d3bbf12a2c7eb23413e
Sreeraj4822 <247577975+Sreeraj4822@users.noreply.github.com>
2026-02-09 15:54:09 +0530

    Added AboutUs component

---
+++ src/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery is a plant store offering a wide variety of indoor and outdoor plants.
+        We are committed to providing high-quality plants and excellent customer service.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
